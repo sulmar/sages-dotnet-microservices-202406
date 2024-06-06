9ee092f312d522fb69d76bf4a71397b8954ea35b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-06-06 15:25:42 +0200

    Zmiana adresu na bramkę Api

--- src/Blazor.Client/Program.cs
+++ src/Blazor.Client/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // TODO: nie hardkoduj!
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7178/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7105/") });
 
 
 await builder.Build().RunAsync();
